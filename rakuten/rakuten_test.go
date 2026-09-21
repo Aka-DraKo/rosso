f9d6fcc9a2380204ac9125894282bd7b9f47d30b
27 <27@riseup.net>
2026-09-21 07:13:07 -0500

    +      url:      "https://rakuten.tv/es/movies/una-obra-maestra",

--- rakuten/rakuten_test.go
+++ rakuten/rakuten_test.go
@@ -9,16 +9,22 @@ var tests = []struct {
    why      []string
 }{
    {
-      height:   1080,
-      language: []string{"CAT", "ENG", "SPA"},
-      url:      "https://rakuten.tv/es/movies/una-obra-maestra",
-      why:      []string{"1080", "language", "/movies/"},
-   },
-   {
       height:   2160,
       language: []string{"ENG"},
       url:      "https://rakuten.tv/ie?content_id=blair-witch&content_type=movies",
       why:      []string{"2160"},
+   },
+   {
+      height:   1080,
+      language: []string{"ENG"},
+      url:      "https://rakuten.tv/uk?content_type=tv_shows&tv_show_id=clink",
+      why:      []string{"tv_show_id="},
+   },
+   {
+      height:   1080,
+      language: []string{"CAT", "ENG", "SPA"},
+      url:      "https://rakuten.tv/es/movies/una-obra-maestra",
+      why:      []string{"1080", "language", "/movies/"},
    },
    {
       height:   1080,
@@ -31,12 +37,6 @@ var tests = []struct {
       language: []string{"CAT", "ENG", "FRA", "ITA", "POR", "SPA"},
       url:      "https://rakuten.tv/nl/tv_shows/matchday",
       why:      []string{"height", "language", "/tv_shows/"},
-   },
-   {
-      height:   1080,
-      language: []string{"ENG"},
-      url:      "https://rakuten.tv/uk?content_type=tv_shows&tv_show_id=clink",
-      why:      []string{"tv_show_id="},
    },
 }
 
